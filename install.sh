842f7de1566ce98c53856859c9dc661d841ac2b4
Dorus Leliveld <dorusleliveld@gmail.com>
2016-10-20 12:41:43 +0200

    added ghci configs

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/Config/nvimrc ~/.config/nvim/init.vim
+ln -s ~/Config/ghci ~/.ghci
 nvim +PlugInstall +qall
